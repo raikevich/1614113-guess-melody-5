b4ffe7bd24474b7ec6b16a576e09345ca1ddd9c0
Nikolay Raikevich <nikolay.raikevich@aventusgroup.com>
2020-10-02 17:45:54 +0300

    module 1, task 2 - Start programm

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/app/app";
+
+const Settings = {
+  ERRORS_COUNT: 3
+};
+
+ReactDOM.render(<App errorsCount={Settings.ERRORS_COUNT} />, document.getElementById(`root`));
