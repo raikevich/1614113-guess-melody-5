2ebcbd348afb6070481f8f824caeb96bbb5faac4
Nikolay Raikevich <nikolay.raikevich@aventusgroup.com>
2020-10-05 18:46:43 +0300

    module 2, task 1 - Route

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,41 @@
 import React from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import PropTypes from "prop-types";
 
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import SignInScreen from "../sign-in-screen/sign-in-screen";
+import ResultScreen from "../result-screen/result-screen";
+import GameOverScreen from "../game-over-screem/game-over-screen";
+import QuestionArtistScreen from "../question-artist-screen/question-artist-screen";
+import QuestionGenreScreen from "../question-genre-screen/question-genre-screen";
 
 const App = (props) => {
   const {errorsCount} = props;
 
-  return <WelcomeScreen errorsCount={errorsCount} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <WelcomeScreen errorsCount={errorsCount}/>
+        </Route>
+        <Route path="/login" exact>
+          <SignInScreen/>
+        </Route>
+        <Route path="/result" exact>
+          <ResultScreen/>
+        </Route>
+        <Route path="/lose" exact>
+          <GameOverScreen/>
+        </Route>
+        <Route path="/dev-artist" exact>
+          <QuestionArtistScreen/>
+        </Route>
+        <Route path="/dev-genre" exact>
+          <QuestionGenreScreen/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
